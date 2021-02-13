d006e3a8df9c772437c925a4697ee436f4b8991b
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2021-02-12 18:49:38 -0600

    chore: add license header to test_use_cases.nim

--- test/test_use_cases.nim
+++ test/test_use_cases.nim
@@ -1,3 +1,13 @@
+#              Task Runner Test Suite
+#               adapted in parts from
+#                Chronos Test Suite
+#
+#            (c) Copyright 2018-Present
+#        Status Research & Development GmbH
+#
+#             Licensed under either of
+#  Apache License, version 2.0, (LICENSE-APACHEv2)
+#             MIT license (LICENSE-MIT)
 import # nim libs
   options, os, random, unittest
 
