a36c2e16701fb70756176cb73d09f90750dbe078
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2021-02-19 12:46:48 -0600

    test: explain in comment why `test_long_running` should be run last

--- test/use_cases/test_all.nim
+++ test/use_cases/test_all.nim
@@ -8,7 +8,13 @@
 #             Licensed under either of
 #  Apache License, version 2.0, (LICENSE-APACHEv2)
 #             MIT license (LICENSE-MIT)
+
+# For now it's important to run `test_long_running` last because of a waku
+# issue re: deletion of port mappings: github.com/status-im/nim-waku/issues/239.
+# If it's run before other tests then there is a consistent hang when exiting
+# whatever test follows the waku test.
+
 import
-  ./test_long_running,
   ./test_short_running_io,
-  ./test_short_running_sync
+  ./test_short_running_sync,
+  ./test_long_running
