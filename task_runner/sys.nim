393c2b6b250ec3016e91979a344915837fbba86f
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2021-02-20 14:31:35 -0600

    refactor: use `allocShared` instead of `createShared`

--- task_runner/sys.nim
+++ task_runner/sys.nim
@@ -2,11 +2,11 @@ type
   ThreadSafeString* = distinct cstring
 
 proc safe*(input: string): ThreadSafeString =
-  var res = cast[cstring](createShared(cstring, input.len + 1))
+  var res = cast[cstring](allocShared(input.len + 1))
   copyMem(res, input.cstring, input.len)
   res[input.len] = '\0'
   res.ThreadSafeString
 
 proc `$`*(input: ThreadSafeString): string =
   result = $(input.cstring)
-  freeShared(cast[ptr cstring](input))
+  deallocShared input.cstring
